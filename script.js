4108c9903b5aa30cdf95b5ede559440176c43622
kprze <kacperej@gmail.com>
2024-09-12 21:53:42 +0100

    Add dialog with form to add new book object

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 const myLibrary = [];
 
-const addBook = document.querySelector("button")
-const bookContainer = document.querySelector(".books")
+const addBtn = document.getElementById("add-book");
+const dialog = document.getElementById("dialog");
+const form = document.getElementById("book-form");
+const submitBtn = dialog.querySelector("#submit-input");
+const closeButton = dialog.querySelector("#cancel-input");
 
-function Book(title,author,pages,status) {
+function Book(title,author,pages,read) {
   // the constructor...
   this.title = title,
   this.author = author,
   this.pages = pages,
-  this.status = status;
+  this.read = read;
 }
+
+addBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  // Check if form is valid before proceeding
+  if (form.checkValidity()) {
+    addBookToLibrary();
+    dialog.close();
+    form.reset();  // Reset form fields after submission
+  } else {
+    form.reportValidity(); // Show validation messages if invalid
+  }
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
 
 function addBookToLibrary() {
-  // do stuff here
-  let title = prompt("Title: ");
-  let author = prompt("Author: ");
-  let pages = prompt("Pages: ");
-  let status = prompt("Read? Y/N: ");
-
-  const book = {title,author,pages,status};
+   // Use FormData to get the form inputs
+   const book = {
+    title: form.title.value,
+    author: form.author.value,
+    pages: form.pages.value,
+    read: form.read.checked
+  };
 
   myLibrary.push(book);
+  console.log(myLibrary)
 }
-
 
 
